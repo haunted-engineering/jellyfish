5e6712b5dace89b3153faa3e48e1bd709282c409
phasewalk1 <phasewalk1@proton.me>
2023-08-06 21:31:06 -0700

    (init): some nice functions

---
+++ clipcat.fish
@@ -0,0 +1,10 @@
+function clipcat
+    if [ "$argv[1]" = "-i" -o "$argv[1]" = "--interactive" ]
+        iclone
+    if test -f $argv
+        cat $argv | xclip -selection clipboard
+        echo "Contents of '$argv' copied to clipboard."
+    else
+        echo "File '$argv' not found."
+    end
+end
