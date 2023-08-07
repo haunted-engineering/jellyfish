5499210c66e21b1d3f81f7e8a6554c88b684602e
phasewalk1 <phasewalk1@proton.me>
2023-08-06 22:13:09 -0700

    (func): +ls +config

--- clipcat.fish
+++ clipcat.fish
@@ -1,6 +1,8 @@
 function clipcat
     if [ "$argv[1]" = "-i" -o "$argv[1]" = "--interactive" ]
         iclone
+    else
+    end
     if test -f $argv
         cat $argv | xclip -selection clipboard
         echo "Contents of '$argv' copied to clipboard."
